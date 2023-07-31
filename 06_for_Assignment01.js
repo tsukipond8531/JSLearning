535c9283342cc2356061cd801c92471634f80125
tsukipond8531 <reader2027@gmail.com>
2023-07-31 09:20:48 +0530

    control flow loop Assignment1 and 2
    control flow loop Assignment1 and 2

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,36 @@
+
+console.log(`-----Print numbers from 5 to 15 by incrementing 1-----`);
+for (let index = 5; index <=15 ; index++) {
+    console.log(index);
+    
+}
+console.log(`-----Print numbers from 50 to 40 by decrementing 1-----`);
+for (let index = 50; index >=40 ; index--) {
+    console.log(index);
+    
+}
+console.log(`-----Find first 15 old number-----`);
+for (let index = 1; index <=29 ; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`-----Find first 10 even number-----`);
+for (let index = 0; index <20 ; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`-----Print table 5 like 5,10,-------,50-----`);
+for (let index = 5; index <=50 ; index=index+5) {
+    console.log(index);
+    
+}
+console.log(`-----Print table 10 like 10,20,-------,100-----`);
+for (let index = 10; index <=100 ; index=index+10) {
+    console.log(index);
+    
+}
+console.log(`-----Print table 10 in reverse like 100,90,-------,10-----`);
+for (let index = 100; index>=10 ; index=index-10) {
+    console.log(index);
+    
+}
---
+++ 06_whileLoop_Assignment01.js
@@ -0,0 +1,47 @@
+
+console.log(`-----Print numbers from 5 to 15 by incrementing 1-----`);
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+    
+}
+console.log(`-----Print numbers from 50 to 40 by decrementing 1-----`);
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i--
+}
+console.log(`-----Find first 15 old number-----`);
+var i=1;
+while (i<=29) {
+    console.log(i);
+    i=i+2;
+    
+}
+console.log(`-----Find first 10 even number-----`);
+var i=0;
+while (i<20) {
+    console.log(i);
+    i=i+2;
+    
+}
+
+console.log(`-----Print table 5 like 5,10,-------,50-----`);
+var i=5;
+while (i<=50) {
+   console.log(i); 
+   i=i+5;
+}
+console.log(`-----Print table 10 like 10,20,-------,100-----`);
+var i=10;
+while (i<=100) {
+   console.log(i); 
+   i=i+10;
+}
+console.log(`-----Print table 10 in reverse like 100,90,-------,10-----`);
+var i=100;
+while (i>=10) {
+   console.log(i); 
+   i=i-10;
+}
