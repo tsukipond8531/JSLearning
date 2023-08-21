3cd62a50b7a50ba8a9a7bd702d63910f14897a85
tsukipond8531 <reader2027@gmail.com>
2023-08-21 08:53:13 +0530

    ES6 features
    ES6 features

---
+++ 15_strickMode.js
@@ -0,0 +1,19 @@
+'use strict';
+// myName = "GK"; // Not allowed
+// console.log(myName);
+
+//var age = 22;
+// delete age; // Not allowed
+
+let student = {
+    fullName: 'Virat Kohli',
+    age: 33,
+    totalRuns: 33000
+}
+
+// delete student; // Not allowed
+
+// function show(num1, num1){
+//     console.log(num1, num1);
+// }
+// show(5, 5);
---
+++ 16_RestParameter.js
@@ -0,0 +1,9 @@
+function add(num1, num2, ...num ){
+    // console.log(`num1 ${num1},  num2 ${num2}`);
+    // console.log(num);
+    //Addition 11
+}
+add(5, 6);
+add(9, 5, 6);
+add(78, 90, 51, 68);
+add(89, 90, 45,78, 90, 51, 68);
